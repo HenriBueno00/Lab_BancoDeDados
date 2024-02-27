826078a8817c8f39c1f279f6f26cef30ea6e2f28
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-02-27 20:00:25 -0300

    Update exercicio01-2.sql

--- EXERCICIOS SQL/GATILHOS/exercicio01-2.sql
+++ EXERCICIOS SQL/GATILHOS/exercicio01-2.sql
@@ -1,11 +1,11 @@
 CREATE TRIGGER empregados_gatilho BEFORE
 INSERT OR UPDATE ON empregados FOR EACH ROW
 	EXECUTE PROCEDURE empregados_gatilho();
-		
-INSERT INTO empregados (codigo,nome, salario) VALUES (5,'João',1000);
-INSERT INTO empregados (codigo,nome, salario) VALUES (6,'José',1500);
-INSERT INTO empregados (codigo,nome, salario) VALUES (7,'Maria',2500);
+INSERT INTO empregados (codigo,nome, salario) 
+	VALUES (5,'João',1000);
+INSERT INTO empregados (codigo,nome, salario) 
+	VALUES (6,'José',1500);
+INSERT INTO empregados (codigo,nome, salario)
+	VALUES (7,'Maria',2500);
 
 SELECT * FROM empregados;
-INSERT INTO empregados (codigo,nome, salario) VALUES (5,NULL,1000);
-INSERT INTO empregados (codigo,nome, salario) VALUES (5,'Andréia',NULL);
