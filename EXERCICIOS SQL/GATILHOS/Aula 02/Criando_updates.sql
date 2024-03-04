6d96998c01f6dd27c458310452eb78e4f8027ad2
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-03-04 19:50:01 -0300

    Create Criando_updates.sql

---
+++ EXERCICIOS SQL/GATILHOS/Aula 02/Criando_updates.sql
@@ -0,0 +1,7 @@
+-- Atualizando a quantidade de estoque de um produto na tabela de produtos
+UPDATE produtos SET quantidade_estoque = 120 WHERE id = 1;
+
+-- Atualizando a quantidade de um pedido na tabela de pedidos
+UPDATE pedidos SET quantidade = 40, valor_total = 400.00 WHERE id = 1;
+
+
