61cdc5f793effb77790c4e985dc69a629bc32eb7
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-05-14 19:37:18 -0300

    .

--- PROJETO/WEB/Index.php
+++ PROJETO/WEB/Index.php
@@ -11,7 +11,7 @@
 <body>
     <h2>Página Inicial</h2>
     <a href="clientes.php"><button>Clientes</button></a>
-    <a href="clientes.php"><button>Produtos</button></a>
+    <a href="Produtos.php"><button>Produtos</button></a>
     <a href="clientes.php"><button>Forma de Pagamento</button></a>
     <a href="clientes.php"><button>Pedidos</button></a>
 </body>
