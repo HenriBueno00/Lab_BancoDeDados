aee940a646e41562de4121b3e5e7771d9ddc51c4
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-05-14 19:34:36 -0300

    .

--- PROJETO/WEB/Index.php
+++ PROJETO/WEB/Index.php
@@ -12,7 +12,7 @@
     <h2>Página Inicial</h2>
     <a href="clientes.php"><button>Clientes</button></a>
     <a href="clientes.php"><button>Produtos</button></a>
-    <a href="clientes.php"><button>Clientes</button></a>
-    <a href="clientes.php"><button>Clientes</button></a>
+    <a href="clientes.php"><button>Forma de Pagamento</button></a>
+    <a href="clientes.php"><button>Pedidos</button></a>
 </body>
 </html>
