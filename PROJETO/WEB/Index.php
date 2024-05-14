52ece1fea7485aee7d8e3a6fcd8c5c99ca86ddb1
Vitor Rodrigues Trindade <vitor.rtrindade@outlook.com>
2024-05-14 20:15:45 -0300

    Adição de menu forma de pagamento

--- PROJETO/WEB/Index.php
+++ PROJETO/WEB/Index.php
@@ -12,7 +12,7 @@
     <h2>Página Inicial</h2>
     <a href="clientes.php"><button>Clientes</button></a>
     <a href="Produtos.php"><button>Produtos</button></a>
-    <a href="clientes.php"><button>Forma de Pagamento</button></a>
+    <a href="FormaPgto.php"><button>Forma de Pagamento</button></a>
     <a href="clientes.php"><button>Pedidos</button></a>
 </body>
 </html>
